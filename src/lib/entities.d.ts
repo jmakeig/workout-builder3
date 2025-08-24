93b880bbb1057c510a7528df7691ce1b5022258a
Justin Makeig <jmpublic@makeig.com>
2025-08-24 23:24:43 +0000

    First crack at entity model Let’s see what Copilot can do…

---
+++ src/lib/entities.d.ts
@@ -0,0 +1,33 @@
+type ID = string & { __brand: 'Identifier' };
+
+/**
+ * Top-level entity. A workout is made of of ordered sets. 
+ */
+export type Workout = {
+    id: ID;
+    name: string;
+    label: string;
+    description: string;
+    sets: Set[];
+};
+
+export type Set = Array<{
+    activity: (Activity & Exercise & { readonly type: 'exercise' }) | Rest;
+    duration: number; // in seconds
+}>;
+
+type Activity = {
+    readonly activity: string;
+}
+
+export type Exercise = {
+    name: string;
+    label: string;
+    description: string | null;
+    instructions: Array<string> | null;
+    alternatives: Array<Exercise> | null;
+};
+
+export type Rest = Activity & {
+    readonly activity: 'rest';
+};
